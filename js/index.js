613f90b94f671b95ccc9be0869df69072783f12f
mike <ritzm220@gmail.com>
2021-05-22 21:35:20 -0400

    Cleaned up header and nav

--- js/index.js
+++ js/index.js
@@ -2,31 +2,37 @@ let previousScrollPos;
 
 window.addEventListener('scroll', () => {
 	let currentScrollPos = window.pageYOffset;
-	if (currentScrollPos > 100) {document.getElementsByTagName('header')[0].classList.add('hide-header');}
-	if (currentScrollPos < 100) {document.getElementsByTagName('header')[0].classList.remove('hide-header');}
-	if (currentScrollPos < previousScrollPos) {document.getElementsByTagName('header')[0].classList.remove('hide-header');}
+
+	if (currentScrollPos > 50) {
+		document.getElementsByTagName('header')[0].classList.add('hide-header');
+		document.getElementsByTagName('header')[0].style.paddingTop = '10px';
+	} else {
+		document.getElementsByTagName('header')[0].classList.remove('hide-header');
+		document.getElementsByTagName('header')[0].style.paddingTop = '30px';
+	}
+
+	if (currentScrollPos < previousScrollPos) document.getElementsByTagName('header')[0].classList.remove('hide-header');
 	previousScrollPos = currentScrollPos ;
+
 });
 
 window.addEventListener('resize', () => {
-	if (window.innerWidth >= 600) {
+	if (window.innerWidth >= 768) {
 		document.getElementById('nav-toggle').checked = false;
 		document.getElementsByTagName('body')[0].style.overflow = 'scroll';
 		document.getElementById('content').classList.remove('blur');
-		document.getElementById('freeze').style.display = 'none';
+		document.getElementById('content-cover').style.display = 'none';
 	}
 });
 
-let navToggle = document.getElementById('nav-toggle');
-
-navToggle.addEventListener('click', (event) => {
+document.getElementById('nav-toggle').addEventListener('click', (event) => {
 	if (event.target.checked) {
 		document.getElementsByTagName('body')[0].style.overflow = 'hidden';
 		document.getElementById('content').classList.add('blur');
-		document.getElementById('freeze').style.display = 'block';
+		document.getElementById('content-cover').style.display = 'block';
 	} else {
 		document.getElementsByTagName('body')[0].style.overflow = 'scroll';
 		document.getElementById('content').classList.remove('blur');
-		document.getElementById('freeze').style.display = 'none';
+		document.getElementById('content-cover').style.display = 'none';
 	}
 });
